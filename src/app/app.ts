e1c8b5e7d62f433f8ae9f47885b894be99a97e36
Leo <leolou.90@gmail.com>
2026-01-01 17:47:03 -0700

    import translate form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateForm } from "../components/translate-form/translate-form";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateForm],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
