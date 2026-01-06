c00a802d38c6010259fad2a99a1a782dad97e531
Leo <leolou.90@gmail.com>
2026-01-05 18:56:15 -0700

    add method to count character to be translated

--- src/components/translate-form/translate-form.ts
+++ src/components/translate-form/translate-form.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'translate-form',
-  imports: [],
+  standalone: true,               
+  imports: [ReactiveFormsModule], 
   templateUrl: './translate-form.html',
   styleUrl: './translate-form.scss',
 })
-export class TranslateForm {
 
-  charactersTyped: number = 0;
 
-  countCharacters(){
-    
+export class TranslateForm implements OnInit {
+
+  readonly MAX_CHARS = 500;
+
+  textArea = new FormControl<string>('', {
+    nonNullable: true,
+    validators: [Validators.maxLength(this.MAX_CHARS)]
+  });
+  charactersTyped = 0;
+
+  //Stop the typing when maximum characters has reach
+  //Can apply some kind of warning for the approach of maximum characters (color change in the counter or a little parragraph)
+  ngOnInit() {
+    this.textArea.valueChanges.subscribe(value => {
+      this.charactersTyped = value?.length ?? 0;
+    });
   }
 }
