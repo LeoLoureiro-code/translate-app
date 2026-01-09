d1cc18c2b22912bce954a2f3a00815869c45b325
Leo <leolou.90@gmail.com>
2026-01-08 18:54:02 -0700

    make text options a standalone component

---
+++ src/components/text-options/text-options.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-options',
+  imports: [],
+  templateUrl: './text-options.html',
+  styleUrl: './text-options.scss',
+})
+export class TextOptions {
+
+}
