e4eb3688169939ac00fc0526ac6eb00f83561803
Leo <leolou.90@gmail.com>
2026-01-08 18:54:35 -0700

    make translate options a standalone component

--- src/components/translate-options/translate-options.ts
+++ src/components/translate-options/translate-options.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-translate-options',
+  selector: 'translate-options',
   imports: [],
   templateUrl: './translate-options.html',
   styleUrl: './translate-options.scss',
