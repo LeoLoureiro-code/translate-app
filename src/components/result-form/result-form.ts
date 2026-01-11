85e75d3808566731d24ac7facd8bd9d3640d64fd
Leo <leolou.90@gmail.com>
2026-01-10 23:33:15 -0700

    add html tags and options components

--- src/components/result-form/result-form.ts
+++ src/components/result-form/result-form.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateOptions } from "../translate-options/translate-options";
+import { TextOptions } from "../text-options/text-options";
 
 @Component({
   selector: 'result-form',
-  imports: [TranslateOptions],
+  imports: [TranslateOptions, TextOptions],
   templateUrl: './result-form.html',
   styleUrl: './result-form.scss',
 })
